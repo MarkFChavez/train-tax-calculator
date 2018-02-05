3060c444c518f37f5bf0ab524026f22242c8798f
Mark Chavez <markchav3z@gmail.com>
2018-02-05 22:41:04 +0800

    Add contribution method to calculator

--- lib/train/tax/calculator.rb
+++ lib/train/tax/calculator.rb
@@ -6,13 +6,19 @@ require "train/tax/calculator/sss"
 module Train
   module Tax
     module Calculator
-      def self.monthly(salary_base)
-        3_000
-      end
 
       def self.for_philhealth(salary_base)
         Philhealth.compute(salary_base)
       end
+
+      def self.for_pagibig(salary_base)
+        Pagibig.compute(salary_base)
+      end
+
+      def self.for_sss(salary_base)
+        Sss.compute(salary_base)
+      end
+
     end
   end
 end
