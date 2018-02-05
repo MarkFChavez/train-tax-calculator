bd10a0680e8659002c1bb8c57a63ee1a8b476a01
Mark Chavez <markchav3z@gmail.com>
2018-02-05 22:39:20 +0800

    Add tests for employer_share

--- spec/train/tax/calculator/sss_spec.rb
+++ spec/train/tax/calculator/sss_spec.rb
@@ -2,35 +2,53 @@ RSpec.describe Train::Tax::Calculator::Sss do
 
   describe ".compute" do
     
-    describe "calculating employee share" do
-      context "with salary base of 15,000" do
-        it "has an employee share of 545.00" do
-          expect(subject.compute_employee_share(15_000)).to eq 545.00
-        end
+    context "with salary base of 15,000" do
+      it "has an employee share of 545.00" do
+        expect(subject.compute_employee_share(15_000)).to eq 545.00
       end
 
-      context "with salary base of 25,000" do
-        it "has an employee share of 581.30" do
-          expect(subject.compute_employee_share(25_000)).to eq 581.30
-        end
+      it "has an employer share of 1,135.00" do
+        expect(subject.compute_employer_share(15_000)).to eq 1_135.00
+      end
+    end
+
+    context "with salary base of 25,000" do
+      it "has an employee share of 581.30" do
+        expect(subject.compute_employee_share(25_000)).to eq 581.30
       end
 
-      context "with salary base of 50,000" do
-        it "has an employee share of 581.30" do
-          expect(subject.compute_employee_share(50_000)).to eq 581.30
-        end
+      it "has an employer share of 1,208.70" do
+        expect(subject.compute_employer_share(25_000)).to eq 1_208.70
+      end
+    end
+
+    context "with salary base of 50,000" do
+      it "has an employee share of 581.30" do
+        expect(subject.compute_employee_share(50_000)).to eq 581.30
       end
 
-      context "with salary base of 80,000" do
-        it "has an employee share of 581.30" do
-          expect(subject.compute_employee_share(80_000)).to eq 581.30
-        end
+      it "has an employer share of 1,208.70" do
+        expect(subject.compute_employer_share(50_000)).to eq 1_208.70
+      end
+    end
+
+    context "with salary base of 80,000" do
+      it "has an employee share of 581.30" do
+        expect(subject.compute_employee_share(80_000)).to eq 581.30
       end
 
-      context "with salary base of 100,000" do
-        it "has an employee share of 581.30" do
-          expect(subject.compute_employee_share(100_000)).to eq 581.30
-        end
+      it "has an employer share of 1,208.70" do
+        expect(subject.compute_employer_share(80_000)).to eq 1_208.70
+      end
+    end
+
+    context "with salary base of 100,000" do
+      it "has an employee share of 581.30" do
+        expect(subject.compute_employee_share(100_000)).to eq 581.30
+      end
+
+      it "has an employer share of 1,208.70" do
+        expect(subject.compute_employer_share(100_000)).to eq 1_208.70
       end
     end
 
